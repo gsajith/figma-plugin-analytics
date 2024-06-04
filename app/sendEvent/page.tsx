a79c199e6d0289f96a01de031d31d4ca2f6c7680
Gautham Sajith <gsajith@umich.edu>
2024-06-04 17:28:45 -0400

    Move suspense wrapper

--- app/sendEvent/page.tsx
+++ app/sendEvent/page.tsx
@@ -3,7 +3,7 @@ import { useSearchParams } from "next/navigation";
 import Script from "next/script";
 import { Suspense, useEffect } from "react";
 
-export default function SendEventPage() {
+function SendEventPage() {
   const searchParams = useSearchParams();
   const eventName = searchParams.get("eventName");
   const gtagID = searchParams.get("gtagID");
@@ -22,10 +22,15 @@ export default function SendEventPage() {
 
   return (
     <div>
-      <Suspense>
-        <Script src="injectGtag.js" />
-        Sending event &quot;{eventName}&quot; to GA4
-      </Suspense>
+      <Script src="injectGtag.js" />
+      Sending event &quot;{eventName}&quot; to GA4
     </div>
   );
 }
+
+const SuspenseWrapper = () => (
+  <Suspense>
+    <SendEventPage />
+  </Suspense>
+);
+export default SuspenseWrapper;
